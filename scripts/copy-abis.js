be0324970a0e70c9030fd75a4a69d0f61595627c
Moshe Levy <lgm@nemesis.co.il>
2021-05-16 21:28:37 +0300

    Deployment env params

--- scripts/copy-abis.js
+++ scripts/copy-abis.js
@@ -6,6 +6,9 @@ const coreBuildContractsDir = `${coreBaseDir}/build/contracts`
 const interfaceBaseDir = '../../justdoit-interface'
 const interfaceContractsAbiDir = `${interfaceBaseDir}/src/constants/abis`
 
+require('dotenv').config({ path: __dirname + `/${coreBaseDir}/.env.public` })
+const UPDATED_CONTRACT_NAME = process.env['UPDATED_CONTRACT_NAME']
+
 const rDirectoryPath = path.join(__dirname, coreBuildContractsDir)
 
 fs.readdir(rDirectoryPath, function (err, files) {
@@ -19,11 +22,15 @@ fs.readdir(rDirectoryPath, function (err, files) {
     if (!file.match('.json')) return
     if (file.match('Migrations')) return
     const json = require(`${coreBuildContractsDir}/${file}`)
+    let finalFile = file
+    if (file.match(UPDATED_CONTRACT_NAME))
+      finalFile = file.split('_')[0] + '.json'
+
     const wDirectoryPath = path.join(
       __dirname,
-      `${interfaceContractsAbiDir}/${file}`,
+      `${interfaceContractsAbiDir}/${finalFile}`,
     )
     fs.writeFileSync(wDirectoryPath, JSON.stringify(json.abi, null, 2))
-    console.log(`Copied: ${file}`)
+    console.log(`Copied: ${file} -> ${finalFile}`)
   })
 })
